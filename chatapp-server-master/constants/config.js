b7db70d2dacdb248a62a6a7e58d3ac79c4a75779
khushi <vermakhushi481@gmail.com>
2025-07-27 00:38:19 +0530

    cors error

--- chatapp-server-master/constants/config.js
+++ chatapp-server-master/constants/config.js
@@ -2,7 +2,8 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://chatting-website-eight.vercel.app", // ✅ Add this
+    "https://chatting-website-eight.vercel.app",
+    "https://talkify-chatapp-realtime.vercel.app", // ✅ Add this
     process.env.CLIENT_URL, // ✅ In case you use env vars too
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
